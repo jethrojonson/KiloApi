2d0b417a9e32df2fa4d08a59960aa00000357153
jethrojonson <biojpg@gmail.com>
2022-12-14 11:02:36 +0100

    Se añaden el proyecto de spring y la coleccion de postman

---
+++ kiloapi/src/test/java/com/salesianostriana/dam/kiloapi/KiloapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.kiloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiloapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
