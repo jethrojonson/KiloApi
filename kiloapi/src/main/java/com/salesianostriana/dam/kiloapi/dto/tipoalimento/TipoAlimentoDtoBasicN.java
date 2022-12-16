ff6d2ce445502463afeef44e00257245cd837fce
Nicoffb <nicoffb@hotmail.com>
2022-12-16 14:22:08 +0100

    cambiado nombre dto

---
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/tipoalimento/TipoAlimentoDtoBasicN.java
@@ -0,0 +1,15 @@
+package com.salesianostriana.dam.kiloapi.dto.tipoalimento;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TipoAlimentoDtoBasic {
+
+    private Long id;
+    private String nombre;
+
+}
