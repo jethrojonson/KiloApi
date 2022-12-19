78776bdc7ca4c887479fa79efd1cdbee8a2baece
gcvictor22 <victorgonzaleztr22@triana.salesianos.edu>
2022-12-19 12:44:25 +0100

    Corrección de documentacion sawgger

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/controller/KilosDisponiblesController.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/controller/KilosDisponiblesController.java
@@ -2,8 +2,15 @@ package com.salesianostriana.dam.kiloapi.controller;
 
 import com.salesianostriana.dam.kiloapi.dto.kilosdisponibles.GetKilosDisponiblesDto;
 import com.salesianostriana.dam.kiloapi.dto.kilosdisponibles.KilosDisponiblesDtoConverter;
+import com.salesianostriana.dam.kiloapi.dto.tipoalimento.TipoAlimentoDtoBasicN;
 import com.salesianostriana.dam.kiloapi.model.KilosDisponibles;
 import com.salesianostriana.dam.kiloapi.service.KilosDisponiblesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +30,32 @@ public class KilosDisponiblesController {
     private final KilosDisponiblesService kilosDisponiblesService;
     private final KilosDisponiblesDtoConverter kilosDisponiblesDtoConverter;
 
+    @Operation(summary = "Obtiene todos los kilos disponibles de todos los alimentos aportados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se han obtenido todos kilos disponibles de los alimentos aportados",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GetKilosDisponiblesDto.class)
+                            , examples = @ExampleObject(
+                            value = """
+                                    [
+                                      {
+                                        "idAlimento": 3,
+                                        "nombreAlimento": "Legumbres",
+                                        "kilosDisponibles": 84.32
+                                      },
+                                      {
+                                        "idAlimento": 4,
+                                        "nombreAlimento": "Frutos secos",
+                                        "kilosDisponibles": 33.6
+                                      }
+                                    ]
+                                    """
+                    ))}),
+            @ApiResponse(responseCode = "404",
+                    description = "No hay existencias de kilos disponibles",
+                    content = @Content),
+    })
     @GetMapping("/")
     public ResponseEntity<List<GetKilosDisponiblesDto>> findAll(){
 
