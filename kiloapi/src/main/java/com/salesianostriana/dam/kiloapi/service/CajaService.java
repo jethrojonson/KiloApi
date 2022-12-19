56399968c51aa481a4119db86f6bfc0d5e33441a
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-12-19 21:57:56 +0100

    Enpoints terminados y funcionando

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/service/CajaService.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/service/CajaService.java
@@ -1,15 +1,14 @@
 package com.salesianostriana.dam.kiloapi.service;
 
-import com.salesianostriana.dam.kiloapi.model.Caja;
-import com.salesianostriana.dam.kiloapi.model.KilosDisponibles;
-import com.salesianostriana.dam.kiloapi.model.TipoAlimento;
+import com.salesianostriana.dam.kiloapi.model.*;
 import com.salesianostriana.dam.kiloapi.repos.CajaRepository;
+import com.salesianostriana.dam.kiloapi.repos.KilosDisponiblesRepository;
 import com.salesianostriana.dam.kiloapi.repos.TipoAlimentoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -17,6 +16,8 @@ import java.util.Optional;
 public class CajaService {
 
     private final CajaRepository cajaRepository;
+    private final KilosDisponiblesRepository kilosDisponiblesRepository;
+    private final TipoAlimentoRepository tipoAlimentoRepository;
 
     private final CajaRepository repo;
 
@@ -28,14 +29,26 @@ public class CajaService {
 
     public Caja save(Caja c) { return repo.save(c); }
 
-    public void deleteById (Long id) { repo.deleteById(id); }
-
+    public void deleteById(Long id) { repo.deleteById(id); }
     public boolean existById (Long id) { return repo.existsById(id); }
 
-    public Caja changeTipoAlimentoAmount (Caja c, TipoAlimento ta, Double kgs){
+    public Caja changeTipoAlimentoAmount (Long idC, Long idA, Double kgs){
+
+        Caja c = cajaRepository.findById(idC).get();
+        TipoAlimento ta = tipoAlimentoRepository.findById(idA).get();
 
         c.getTieneList().forEach(t -> {
-            if(Objects.equals(t.getTipoAlimento().getId(), ta.getId())){
+            if(t.getTipoAlimento().equals(ta) && kgs < t.getTipoAlimento().getKilosDisponibles().getCantidadDisponible()){
+                ResponseEntity.of(
+                        kilosDisponiblesRepository.findById(ta.getId())
+                                .map(old -> {
+                                    old.setTipoAlimento(t.getTipoAlimento());
+                                    old.setId(t.getTipoAlimento().getId());
+                                    old.setCantidadDisponible(old.getCantidadDisponible() + t.getCantidadKgs() - kgs);
+                                    return kilosDisponiblesRepository.save(old);
+                                })
+                );
+                c.setKilosTotales(c.getKilosTotales() + kgs - t.getCantidadKgs());
                 t.setCantidadKgs(kgs);
                 repo.save(c);
             }
@@ -43,6 +56,22 @@ public class CajaService {
 
         return c;
 
+    }
+
+    public Long preRemoveAlimentos (Long id) {
+
+        Caja caja = cajaRepository.findById(id).get();
+
+        caja.getTieneList().forEach(tiene -> {
+            kilosDisponiblesRepository.findAll().forEach(k -> {
+                if(tiene.getTipoAlimento().equals(k.getTipoAlimento())){
+                    k.setCantidadDisponible(k.getCantidadDisponible()+tiene.getCantidadKgs());
+                    kilosDisponiblesRepository.save(k);
+                }
+            });
+        });
+
+        return caja.getId();
     }
 
 }
