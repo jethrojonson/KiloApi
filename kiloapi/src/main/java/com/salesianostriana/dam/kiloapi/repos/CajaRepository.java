ffe0f41b40796e095ff2a0a428748019e9f1602c
Nico <nicoffb@hotmail.com>
2022-12-21 19:15:40 +0100

    retoques finales

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/repos/CajaRepository.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/repos/CajaRepository.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.kiloapi.repos;
 
+import com.salesianostriana.dam.kiloapi.dto.caja.CajaDtoN;
 import com.salesianostriana.dam.kiloapi.dto.caja.CajaTipoAlimentoDto;
 import com.salesianostriana.dam.kiloapi.model.Caja;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +15,11 @@ public interface CajaRepository extends JpaRepository<Caja,Long> {
             WHERE t.caja.id = ?1
             """)
     List<CajaTipoAlimentoDto> findAlimentosOfACaja(Long idCaja);
+
+    @Query("""
+            SELECT NEW com.salesianostriana.dam.kiloapi.dto.caja.CajaDtoN(c.id, c.qr, c.numCaja, c.kilosTotales)
+            FROM Caja c
+            """)
+    List<CajaDtoN> findAllCajas();
 
 }
--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/service/TipoAlimentoService.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/service/TipoAlimentoService.java
@@ -4,6 +4,7 @@ import com.salesianostriana.dam.kiloapi.dto.tipoalimento.TipoAlimentoDto;
 import com.salesianostriana.dam.kiloapi.dto.tipoalimento.TipoAlimentoDtoBasicN;
 import com.salesianostriana.dam.kiloapi.model.KilosDisponibles;
 import com.salesianostriana.dam.kiloapi.model.TipoAlimento;
+import com.salesianostriana.dam.kiloapi.repos.AportacionRepository;
 import com.salesianostriana.dam.kiloapi.repos.KilosDisponiblesRepository;
 import com.salesianostriana.dam.kiloapi.repos.TipoAlimentoRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class TipoAlimentoService {
 
     private final TipoAlimentoRepository repo;
     private final KilosDisponiblesRepository kilosDisponiblesRepository;
+    private final AportacionRepository aportacionRepository;
 
     public List<TipoAlimento> findAll(){ return repo.findAll(); }
 
@@ -41,6 +44,33 @@ public class TipoAlimentoService {
         kilosDisponiblesRepository.save(kilos);
         TipoAlimentoDtoBasicN response = TipoAlimentoDtoBasicN.of(nuevo);
         return response;
+    }
+
+    public TipoAlimento findTipoAlimentoOnTiene(Long id){
+        return repo.findTipoAlimentoOnTiene(id);
+    }
+
+    public TipoAlimento findTipoAlimentoOnDetallesAportacion(Long id){
+        return  repo.findTipoAlimentoOnDetallesAportacion(id);
+    }
+
+    public boolean comprobarBorradoTipoAlimento(Long id){
+        TipoAlimento t = findById(id).get();
+        AtomicBoolean comprobacion = new AtomicBoolean(false);
+        final double[] numero = {0.0};
+        aportacionRepository.findAll().forEach(aportacion -> {
+            aportacion.getDetalles().forEach(detalleAportacion -> {
+                if (detalleAportacion.getTipoAlimento().equals(t)) {
+                    comprobacion.set(true);
+                }
+            });
+        });
+        kilosDisponiblesRepository.findAll().forEach(kilosDisponibles -> {
+            if (kilosDisponibles.getTipoAlimento().equals(t)){
+                numero[0] =kilosDisponibles.getCantidadDisponible();
+            }
+        });
+        return !comprobacion.get() && numero[0]==0.0;
     }
     
 }
