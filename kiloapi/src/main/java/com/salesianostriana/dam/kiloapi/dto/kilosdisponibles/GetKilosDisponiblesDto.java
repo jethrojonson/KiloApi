518448ca5fd681c1e79a0c06355067290c288c23
josgilmar2 <josgilmar2@gmail.com>
2022-12-21 09:41:45 +0100

    Se hace json view de kilos disponibles del detalle

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/kilosdisponibles/GetKilosDisponiblesDto.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/kilosdisponibles/GetKilosDisponiblesDto.java
@@ -2,15 +2,18 @@ package com.salesianostriana.dam.kiloapi.dto.kilosdisponibles;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.salesianostriana.dam.kiloapi.dto.detalleaportacion.DetalleAportacionDto;
 import com.salesianostriana.dam.kiloapi.views.KilosDisponiblesViews;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@SuperBuilder
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GetKilosDisponiblesDto {
 
@@ -22,5 +25,9 @@ public class GetKilosDisponiblesDto {
 
     @JsonView(KilosDisponiblesViews.Master.class)
     private Double kilosDisponibles;
+
+    @JsonView(KilosDisponiblesViews.Details.class)
+    private List<DetalleAportacionDto> aportaciones;
+
 
 }
